80d1fd502d68c7ad2596b7c0ad1c1f407dee5af1
yiwanghuang <yiwanghuang97@gmail.com>
2024-06-02 01:01:33 +0800

    create

---
+++ posts/randomStudents/randomGenerator.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+randomList = np.random.choice(np.arange(1,44,1),size=3,replace=False)
+
+# 写入文档
+file_path = 'random_numbers.txt'
+with open(file_path, 'w') as file:
+    file.write(f'面批学号: {randomList}')
+
+# 输出写入文件的结果
+print(f'文件 {file_path} 成功写入！')
